8096bef0660a60b0c26c981026ad10c629dfb625
mithu <nilchirkutpoison@gmail.com>
2024-04-18 00:34:03 +0600

    define category added

--- enroll/urls.py
+++ enroll/urls.py
@@ -3,6 +3,14 @@ from enroll import views
 
 urlpatterns = [
     path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name="product_detail_page"),
+    
+    path('audi/', views.audi_car, name='audiPage'),
+    path('audi/<slug:data>', views.audi_car, name='audiPageData'),
+    
+    path('bugatti/', views.bugatti_car, name='bugattiPage'),
+    path('bugatti/<slug:data>', views.bugatti_car, name='bugattiPageData'),
+    
+    
 ]
 
 
--- enroll/views.py
+++ enroll/views.py
@@ -28,3 +28,30 @@ class ProductDetailView(View):
 
 
 
+# Category for Audi & Bugatti
+def audi_car(request, data=None):
+    if data == None:
+        audis = ProductInfo.objects.filter(category="Audi")
+    elif data == 'Audi_Fuel' or data == 'Audi_Electric':
+        audis = ProductInfo.objects.filter(category="Audi").filter(brand=data)
+    elif data == 'Below':
+        audis = ProductInfo.objects.filter(category="Audi").filter(discounted_price__lt=4200000)
+    elif data == 'Above':
+        audis = ProductInfo.objects.filter(category="Audi").filter(discounted_price__gt=4200000)
+    return render(request, 'enroll/audi_car.html', {"audis":audis})
+
+
+def bugatti_car(request, data=None):
+    if data == None:
+        bugattis = ProductInfo.objects.filter(category="Bugatti")
+    elif data == 'Bugatti_Fuel' or data == 'Bugatti_Electric':
+        bugattis = ProductInfo.objects.filter(category="Bugatti").filter(brand=data)
+    elif data == 'Below':
+        bugattis = ProductInfo.objects.filter(category="Bugatti").filter(discounted_price__lte=9500000)
+    elif data == 'Above':
+        bugattis = ProductInfo.objects.filter(category="Bugatti").filter(discounted_price__gt=9500100)
+    return render(request, 'enroll/bugatti_car.html', {"bugattis":bugattis})
+
+
+
+
