5b885d616ec2f3511e1adc1846bebd9cc2d6841f
mithu <nilchirkutpoison@gmail.com>
2024-04-17 11:34:21 +0600

    Work display home page

--- enroll/views.py
+++ enroll/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import ProductInfo
 
 # Create your views here.
 class HomeTemplateView(TemplateView):
     template_name = 'enroll/home.html'
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        audi = ProductInfo.objects.filter(category="Audi")
+        bmw = ProductInfo.objects.filter(category='BMW')
+        bugatti = ProductInfo.objects.filter(category="Bugatti")
+        mercedes = ProductInfo.objects.filter(category="Mercedes")
+        range_rover = ProductInfo.objects.filter(category="Range Rover")
+        context = {'audi':audi, 'bmw':bmw, 'bugatti':bugatti, 'mercedes':mercedes, 'range_rover':range_rover}
+        return context
+
+
+
